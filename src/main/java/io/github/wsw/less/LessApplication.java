f98d66808282f79075ad0a2e1e485af7b110d5ee
weishuwen <870224929@qq.com>
2019-04-26 17:39:07 +0800

    init

---
+++ src/main/java/io/github/wsw/less/LessApplication.java
@@ -0,0 +1,13 @@
+package io.github.wsw.less;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessApplication.class, args);
+	}
+
+}
